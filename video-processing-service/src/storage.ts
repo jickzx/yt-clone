56e51f10758e149928a6fc57cd30c4f045a7a6d0
James Espinosa <psyje12@nottingham.ac.uk>
2026-07-18 20:19:40 +0100

    added storage

---
+++ video-processing-service/src/storage.ts
@@ -0,0 +1,11 @@
+// gcs file interactions & local file interactions 
+
+import { Storage } from '@google-cloud/storage';
+import fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+
+// creates local directories for raw and processed videos
+
+export function setupDirectories() {
+
+}
